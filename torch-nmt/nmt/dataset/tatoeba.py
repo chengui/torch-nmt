39e79913eb8c5d12c52207136553e85fbd5b1b80
Chen Gui <gui.g.chen@gmail.com>
2022-03-21 20:14:55 +0800

    Add tatoeba testset
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/dataset/tatoeba.py
+++ torch-nmt/nmt/dataset/tatoeba.py
@@ -3,15 +3,22 @@ from torch.utils.data import Dataset
 
 
 class TatoebaDataset(Dataset):
-    def __init__(self, root_dir='.', split='train', lang_pair=('eng', 'fra'), processor=None):
+    def __init__(self, root_dir='.', split='train', lang_pair=('eng', 'fra'),
+                 processor=None, test_samples=3200):
         self.src_lang, self.tgt_lang = lang_pair
         self.src_text, self.tgt_text = self._read_text(root_dir, lang_pair)
         if isinstance(processor, (list, tuple)):
             src_processor, tgt_processor = processor
         else:
             src_processor, tgt_processor = processor, processor
-        self.src_data, self.src_vocab = src_processor(self.src_text[:-3200])
-        self.tgt_data, self.tgt_vocab = tgt_processor(self.tgt_text[:-3200])
+        if split == 'train':
+            src_text = self.src_text[:-test_samples]
+            tgt_text = self.tgt_text[:-test_samples]
+        else:
+            src_text = self.src_text[test_samples:]
+            tgt_text = self.tgt_text[test_samples:]
+        self.src_data, self.src_vocab = src_processor(src_text)
+        self.tgt_data, self.tgt_vocab = tgt_processor(tgt_text)
 
     def __getitem__(self, idx):
         return (self.src_data[idx], self.tgt_data[idx])
