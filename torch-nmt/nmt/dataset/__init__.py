a95fc54676858182ee375151e75cf11e5479dd9f
Chen Gui <gui.g.chen@gmail.com>
2022-03-22 11:19:30 +0800

    Fix parameter name
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/dataset/__init__.py
+++ torch-nmt/nmt/dataset/__init__.py
@@ -5,8 +5,8 @@ DATASETS = {
     'tatoeba': TatoebaDataset,
 }
 
-def split_dataset(dataset, lengths=[0.8, 0.2], **kwargs):
-    lengths = [int(len(dataset) * i) for i in lengths]
+def split_dataset(dataset, ratios=[0.8, 0.2]):
+    lengths = [int(len(dataset) * i) for i in ratios]
     lengths[-1] = len(dataset) - sum(lengths[:-1])
     return random_split(dataset, lengths=lengths)
 
