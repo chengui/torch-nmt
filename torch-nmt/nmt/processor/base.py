9784d3cc91224fe3990f951ef4b3b45de270fe6f
Chen Gui <gui.g.chen@gmail.com>
2022-03-22 18:02:36 +0800

    Fix processor
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/processor/base.py
+++ torch-nmt/nmt/processor/base.py
@@ -29,8 +29,11 @@ class Processor(object):
 
 
 class BaseProcessor(Processor):
-    def __init__(self, preprocess=None, tokenize=None):
+    def __init__(self, max_seqlen=None, min_wordfreq=2,
+                 preprocess=None, tokenize=None):
         super().__init__()
+        self.max_seqlen = max_seqlen
+        self.min_wordfreq = min_wordfreq
         self._preprocess = preprocess
         self._tokenize = tokenize
 
@@ -60,7 +63,9 @@ class BaseProcessor(Processor):
             _tokenize = space_split
         return [_tokenize(text) for text in texts]
 
-    def build_vocab(self, tokens, min_freq=2):
+    def build_vocab(self, tokens, min_freq=None):
+        if min_freq is None:
+            min_freq = self.min_wordfreq
         vocab = Vocab()
         vocab.build(tokens, min_freq)
         return vocab
@@ -68,10 +73,14 @@ class BaseProcessor(Processor):
     def numerical(self, tokens, vocab, maxlen=None):
         def pad(tokens, maxlen):
             pad_length = max(0, maxlen-len(tokens))
-            pad_tokens = tokens[:maxlen] + [Vocab.PAD_TOK] * pad_length
-            return [Vocab.SOS_TOK] + pad_tokens + [Vocab.EOS_TOK]
-        if not maxlen:
-            maxlen = max(len(l) for l in tokens)
-        tokens = [pad(words, maxlen) for words in tokens]
+            pad_tokens = [Vocab.SOS_TOK] + tokens + [Vocab.EOS_TOK]
+            pad_tokens = tokens + [Vocab.PAD_TOK] * pad_length
+            return pad_tokens[:maxlen]
+        if maxlen is None:
+            if self.max_seqlen:
+                maxlen = self.max_seqlen
+            else:
+                maxlen = max(len(l) for l in tokens)
+        tokens = [pad(words, maxlen+2) for words in tokens]
         vector = [vocab[words] for words in tokens]
         return torch.tensor(vector)
