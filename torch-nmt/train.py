1cf3299f41beed12f6244b2f3fadb52b75123694
Chen Gui <gui.g.chen@gmail.com>
2022-03-22 11:20:08 +0800

    Fix train script
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/train.py
+++ torch-nmt/train.py
@@ -1,37 +1,42 @@
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
-from datasets import create_dataset, split_dataset
-from models import create_model
-from utils import plot_history, bleu_score
-from utils import load_model, save_model
+from nmt.processor.base import BaseProcessor
+from nmt.model import create_model
+from nmt.dataset import (
+    create_dataset,
+    split_dataset
+)
+from nmt.util.fs_util import (
+    load_checkpoint,
+    save_checkpoint,
+    save_vocab
+)
 
 
-def valid_epoch(model, data_iter, vocab):
+def valid_epoch(model, data_iter, criterion):
+    model.eval()
     with torch.no_grad():
-        valid_bleu = 0.0
+        valid_loss = 0
         for idx, (src, tgt) in enumerate(data_iter):
             pred = model(src, tgt)
-            # pred: [seq_len, batch_size, vocab_size]
-            # tgt: [batch_size, seq_len]
-            pred = pred.argmax(2).permute(1, 0)
-            # pred: [batch_size, seq_len]
-            pred = [vocab.token(ids) for ids in pred]
-            tgt = [vocab.token(ids) for ids in tgt]
-            bleu = bleu_score(pred, tgt)
-            valid_bleu += bleu
-        valid_bleu /= len(data_iter)
-        return valid_bleu
+            # pred: (batch_size, seq, vocab_size)
+            # tgt: (batch_size, seq)
+            pred = pred.permute(0, 2, 1)
+            loss = criterion(pred[:, :, 1:], tgt[:, 1:])
+            valid_loss += loss
+        valid_loss /= len(data_iter)
+        return valid_loss
 
-def train_epoch(model, data_iter, optimizer, criterion):
+def train_epoch(model, data_iter, criterion, optimizer):
+    model.train()
     train_loss = 0
     for idx, (src, tgt) in enumerate(data_iter):
         optimizer.zero_grad()
-
         pred = model(src, tgt)
-        # pred: [seq_len, batch_size, vocab_size]
-        pred = pred.permute(1, 2, 0)
-
+        # pred: (batch_size, seq, vocab_size)
+        # tgt: (batch_size, seq)
+        pred = pred.permute(0, 2, 1)
         loss = criterion(pred[:, :, 1:], tgt[:, 1:])
         loss.backward()
         #clip_grad
@@ -41,37 +46,31 @@ def train_epoch(model, data_iter, optimizer, criterion):
     train_loss /= len(data_iter)
     return train_loss
 
-def train(model, dataset, num_epochs, batch_size, lr, pretrain, output):
+def train(model, dataset, num_epochs, batch_size, lr, pretrain, outdir):
     src_vocab, tgt_vocab =  dataset.src_vocab, dataset.tgt_vocab
-    trainset, validset = split_dataset(dataset, lengths=[0.8, 0.2])
+    trainset, validset = split_dataset(dataset, ratios=[0.8, 0.2])
 
     train_iter = DataLoader(dataset=trainset, batch_size=batch_size)
     valid_iter = DataLoader(dataset=validset, batch_size=batch_size)
 
-    criterion = nn.CrossEntropyLoss(ignore_index=tgt_vocab['<pad>'])
+    criterion = nn.CrossEntropyLoss(ignore_index=tgt_vocab.PAD_IDX)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     if pretrain:
-        load_model(pretrain, model, optimizer)
-    else:
-        model.apply(init_weights)
+        load_checkpoint(pretrain, model, optimizer)
 
-    loss_lst, bleu_lst = [], []
+    train_hist, valid_hist = [], []
     for epoch in range(num_epochs):
-        train_loss = train_epoch(model, train_iter, optimizer, criterion)
-        valid_bleu = valid_epoch(model, valid_iter, tgt_vocab)
+        train_loss = train_epoch(model, train_iter, criterion, optimizer)
+        valid_loss = valid_epoch(model, valid_iter, criterion)
 
-        loss_lst.append(train_loss)
-        bleu_lst.append(valid_bleu)
-        print(f'epoch {epoch + 1}: loss={train_loss:>3f}, bleu={valid_bleu:>3f}')
+        train_hist.append(train_loss)
+        valid_hist.append(valid_loss)
+        print(f'epoch {epoch + 1}: train_loss={train_loss:>3f}, '
+              f'valid_loss={valid_loss:>3f}')
 
-    save_model(output, model, optimizer, src_vocab, tgt_vocab)
-    plot_history(output,
-                 legend=['train loss', 'valid bleu'],
-                 plots=[loss_lst, bleu_lst])
-
-def init_weights(m):
-    for name, param in m.named_parameters():
-        nn.init.uniform_(param.data, -0.08, 0.08)
+        if (epoch + 1) % 10 == 0:
+            save_checkpoint(f'{outdir}/model.pt', model, optimizer)
+            save_vocab(src_vocab, tgt_vocab, outdir)
 
 if __name__ == '__main__':
     import argparse
@@ -89,16 +88,17 @@ if __name__ == '__main__':
                         help='number of train epochs')
     parser.add_argument('-r', '--root-dir', default='.',
                          help='root dir of dataset')
-    parser.add_argument('-t', '--pretrain', default='classify.pt',
+    parser.add_argument('-t', '--pretrain', default='nmt.pt',
                          help='pretrain model')
-    parser.add_argument('-o', '--output', default='classify.pt',
-                        help='output model')
+    parser.add_argument('-o', '--outdir', default='./output',
+                        help='output dir')
     args = parser.parse_args()
 
     dataset = create_dataset(dataset=args.dataset,
-                             root_dir=args.root_dir)
+                             root_dir=args.root_dir,
+                             processor=BaseProcessor())
     vocab_size = (len(dataset.src_vocab), len(dataset.tgt_vocab))
     model = create_model(model=args.model,
                          vocab_size=vocab_size)
 
-    train(model, dataset, args.epochs, args.batch_size, args.lr, args.pretrain, args.output)
+    train(model, dataset, args.epochs, args.batch_size, args.lr, args.pretrain, args.outdir)
