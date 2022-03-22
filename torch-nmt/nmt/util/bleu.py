fe70f6b3046a833f93ca62b5eea8876effad6551
Chen Gui <gui.g.chen@gmail.com>
2022-03-22 17:08:00 +0800

    Fix short sentence bleu
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/util/bleu.py
+++ torch-nmt/nmt/util/bleu.py
@@ -9,6 +9,12 @@ def bleu_score(conds, refs, max_n=4, weights=[0.25]*4):
         cond_len += len(cond)
         ref_len += len(ref)
         for n in range(max_n):
+            if len(ref) <= n:
+                num_match[n] = -1
+                break
+            if len(cond) <= n:
+                num_total[n] = 1
+                continue
             num_total[n] += len(cond) - n
             ref_counter = collections.defaultdict(int)
             for i in range(len(ref)-n):
@@ -19,6 +25,8 @@ def bleu_score(conds, refs, max_n=4, weights=[0.25]*4):
                     ref_counter[' '.join(cond[i:i+n+1])] -= 1
     score = math.exp(min(0, 1-ref_len/cond_len))
     for i in range(max_n):
+        if num_match[i] < 0:
+            break
         score *= math.pow(num_match[i]/num_total[i], weights[i])
     return score
 
