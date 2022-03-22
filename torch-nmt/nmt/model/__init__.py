32e3c787abdae839399e0602680ca54fcf2c995d
Chen Gui <gui.g.chen@gmail.com>
2022-03-22 11:16:57 +0800

    Add batch_first for rnn models
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/model/__init__.py
+++ torch-nmt/nmt/model/__init__.py
@@ -1,12 +1,18 @@
+from torch import nn
 from nmt.model.seq2seq_rnn import EncoderRNN, DecoderRNN, Seq2SeqRNN
 
 MODELS = {
     'rnn': (EncoderRNN, DecoderRNN, Seq2SeqRNN),
 }
 
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.uniform_(param.data, -0.08, 0.08)
+
 def create_model(model, vocab_size, embed_size=32, num_hiddens=32, num_layers=2, dropout=0.1):
     (Encoder, Decoder, Seq2Seq) = MODELS[model]
     encoder = Encoder(vocab_size[0], embed_size, num_hiddens, num_layers, dropout)
     decoder = Decoder(vocab_size[1], embed_size, num_hiddens, num_layers, dropout)
     seq2seq = Seq2Seq(encoder, decoder)
+    seq2seq.apply(init_weights)
     return seq2seq
--- torch-nmt/nmt/model/seq2seq_rnn.py
+++ torch-nmt/nmt/model/seq2seq_rnn.py
@@ -4,42 +4,46 @@ from torch import nn
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
+                 dropout):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, embed_size)
-        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers,
+                          batch_first=True, dropout=dropout)
 
     def forward(self, X):
-        # X: [seq_len, batch_size]
+        # X: (batch_size, seq)
         X = self.embedding(X)
-        # X: [seq_len, batch_size, embed_size]
+        # X: (batch_size, seq, embed_size)
         Y, state = self.rnn(X)
-        # Y: [seq_len, batch_size, num_hiddens]
+        # Y: [batch_size, seq, num_hiddens]
         # state: [batch_size, num_hiddens]
         return Y, state
 
 class DecoderRNN(nn.Module):
-    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
+                 dropout):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, embed_size)
-        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers,
+                          batch_first=True, dropout=dropout)
         self.dense = nn.Linear(num_hiddens, vocab_size)
 
     def forward(self, X, state):
-        # X: [batch_size]
-        X = X.unsqueeze(0)
-        # X: [1, batch_size]
+        # X: (batch_size)
+        X = X.unsqueeze(1)
+        # X: (batch_size, 1)
         X = self.embedding(X)
-        # X: [1, batch_size, embed_size]
+        # X: (batch_size, 1, embed_size)
         Y, state = self.rnn(X, state)
-        # Y: [1, batch_size, num_hiddens]
-        # state: [batch_size, num_hiddens]
+        # Y: (batch_size, 1, num_hiddens)
+        # state: (batch_size, num_hiddens)
         Y = self.dense(Y)
-        # Y: [1, batch_size, vocab_size]
-        Y = Y.squeeze(0)
-        # Y: [batch_size, vocab_size]
+        # Y: (batch_size, 1, vocab_size)
+        Y = Y.squeeze(1)
+        # Y: (batch_size, vocab_size)
         return Y, state
 
 class Seq2SeqRNN(nn.Module):
@@ -48,24 +52,20 @@ class Seq2SeqRNN(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self, enc_X, dec_X, teacher_ratio=1.0):
-        # enc_X: [batch_size, seq_len]
-        # dec_X: [batch_size, seq_len]
-        enc_X = enc_X.permute(1, 0)
-        dec_X = dec_X.permute(1, 0)
-        # enc_X: [seq_len, batch_size]
-        # dec_X: [seq_len, batch_size]
-        batch_size = dec_X.shape[1]
-        seq_len = dec_X.shape[0]
-        # enc_X: [seq_len, batch_size]
+    def forward(self, enc_X, dec_X, teacher_ratio=0.5):
+        # enc_X: (batch_size, seq)
+        # dec_X: (batch_size, seq)
+        vocab_size = self.decoder.vocab_size
+        batch_size = dec_X.shape[0]
+        seq_len = dec_X.shape[1]
         _, state = self.encoder(enc_X)
-        # state: [batch_size, num_hiddens]
-        outputs = torch.zeros(seq_len, batch_size, self.decoder.vocab_size)
-        X = dec_X[0, :]
+        # state: (batch_size, num_hiddens)
+        outputs = [torch.zeros(batch_size, 1, vocab_size)]
+        X = dec_X[:, 0]
         for t in range(1, seq_len):
             output, state = self.decoder(X, state)
-            outputs[t] = output
+            outputs.append(output.unsqueeze(1))
             pred = output.argmax(1)
-            X = dec_X[t] if random.random() < teacher_ratio else pred
-        # outputs: [seq_len, batch_size, vocab_size]
-        return outputs
+            X = dec_X[:, t] if random.random() < teacher_ratio else pred
+        # outputs: (batch_size, seq, vocab_size)
+        return torch.cat(outputs, dim=1)
