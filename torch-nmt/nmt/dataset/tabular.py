dd569a7209b9750401b49ce5268cbb215982f284
Chen Gui <gui.g.chen@outlook.com>
2022-04-07 18:40:08 +0800

    Fix parameter
    Signed-off-by: Chen Gui <gui.g.chen@outlook.com>

--- torch-nmt/nmt/dataset/tabular.py
+++ torch-nmt/nmt/dataset/tabular.py
@@ -7,8 +7,8 @@ class TabularDataset(Dataset):
         super().__init__(**kw)
         src_toks, tgt_toks = read_tsv(path)
         self.examples = list(zip(src_toks, tgt_toks))
-        self.src_data, self.src_len = numerical(src_toks)
-        self.tgt_data, self.tgt_len = numerical(tgt_toks)
+        self.src_data, self.src_len = numerical(src_toks, src_vocab)
+        self.tgt_data, self.tgt_len = numerical(tgt_toks, tgt_vocab)
 
     def __getitem__(self, i):
         return (self.src_data[i], self.src_len[i],
