0a7cdae922cc1f61d62cc20da03b665b40e45fe8
Chen Gui <gui.g.chen@outlook.com>
2022-04-07 16:29:16 +0800

    Move load_ckpt and save_ckpt to model module
    Signed-off-by: Chen Gui <gui.g.chen@outlook.com>

--- torch-nmt/nmt/model/__init__.py
+++ torch-nmt/nmt/model/__init__.py
@@ -1,4 +1,5 @@
-from torch import nn
+import os
+import torch
 from nmt.model.seq2seq_gru import Seq2SeqGRU
 from nmt.model.seq2seq_bahdanau import Seq2SeqBahdanau
 
@@ -9,11 +10,23 @@ MODELS = {
 
 def init_weights(m):
     for name, param in m.named_parameters():
-        nn.init.uniform_(param.data, -0.08, 0.08)
+        torch.nn.init.xavier_uniform_(param.data)
 
-def create_model(model, vocab_size):
-    Seq2Seq = MODELS[model]
-    (enc_vocab, dec_vocab) = vocab_size
-    seq2seq = Seq2Seq(enc_vocab, dec_vocab)
+def create_model(model, enc_vocab, dec_vocab, **kw):
+    seq2seq = MODELS[model](enc_vocab, dec_vocab)
     seq2seq.apply(init_weights)
     return seq2seq
+
+def load_ckpt(model, work_dir, mode='last'):
+    model_file = os.path.join(work_dir, f'checkpoint-{mode}.pt')
+    if not os.path.exists(model_file):
+        raise OSError(f'checkpoint not under {work_dir}')
+    checkpoint = torch.load(model_file)
+    model.load_state_dict(checkpoint['state_dict'])
+
+def save_ckpt(work_dir, model, mode='last'):
+    model_file = os.path.join(work_dir, f'checkpoint-{mode}.pt')
+    checkpoint = {
+        'state_dict': model.state_dict(),
+    }
+    torch.save(checkpoint, model_file)
