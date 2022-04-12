18ac458ecc20130573c3f599e1e0bc140fe660eb
Chen Gui <gui.g.chen@outlook.com>
2022-04-12 16:58:44 +0800

    Rename parameters
    Signed-off-by: Chen Gui <gui.g.chen@outlook.com>

--- torch-nmt/nmt/model/seq2seq_transformer.py
+++ torch-nmt/nmt/model/seq2seq_transformer.py
@@ -3,26 +3,26 @@ from torch import nn
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_hidden, n_head, dropout=0.1):
+    def __init__(self, n_hiddens, n_heads, dropout):
         super().__init__()
-        self.n_head = n_head
-        self.h_hidden = n_hidden // n_head
-        self.w_q = nn.Linear(n_hidden, n_hidden)
-        self.w_k = nn.Linear(n_hidden, n_hidden)
-        self.w_v = nn.Linear(n_hidden, n_hidden)
-        self.w_o = nn.Linear(n_hidden, n_hidden)
+        self.n_heads = n_heads
+        self.h_hiddens = n_hiddens // n_heads
+        self.w_q = nn.Linear(n_hiddens, n_hiddens)
+        self.w_k = nn.Linear(n_hiddens, n_hiddens)
+        self.w_v = nn.Linear(n_hiddens, n_hiddens)
+        self.w_o = nn.Linear(n_hiddens, n_hiddens)
         self.dropout = nn.Dropout(dropout)
-        self.scale = torch.sqrt(torch.tensor([self.h_hidden]))
+        self.scale = torch.sqrt(torch.FloatTensor([self.h_hiddens]))
 
     def forward(self, q, k, v, mask=None):
         bs, ls, hs = q.shape
         # q, k, v: (batch, srclen/tgtlen, hidden)
         q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
         # q, k, v: (batch, srclen/tgtlen, hidden)
-        q = q.view(bs, -1, self.n_head, self.h_hidden).transpose(1, 2)
-        k = k.view(bs, -1, self.n_head, self.h_hidden).transpose(1, 2)
-        v = v.view(bs, -1, self.n_head, self.h_hidden).transpose(1, 2)
-        # q, k, v: (batch, head, srclen/tgtlen, h_hidden)
+        q = q.view(bs, -1, self.n_heads, self.h_hiddens).transpose(1, 2)
+        k = k.view(bs, -1, self.n_heads, self.h_hiddens).transpose(1, 2)
+        v = v.view(bs, -1, self.n_heads, self.h_hiddens).transpose(1, 2)
+        # q, k, v: (batch, head, srclen/tgtlen, h_hiddens)
         e = torch.matmul(q, k.transpose(2, 3)) / self.scale
         # e: (batch, head, seqlen, seqlen)
         if mask is not None:
@@ -30,30 +30,30 @@ class MultiHeadAttention(nn.Module):
         a = torch.softmax(e, dim=-1)
         # a: (batch, head, seqlen, seqlen)
         z = torch.matmul(self.dropout(a), v)
-        z = z.transpose(1, 2).reshape(bs, -1, hs)
+        z = z.transpose(1, 2).contiguous().view(bs, -1, hs)
         # z: (batch, seqlen, hidden)
         o = self.w_o(z)
         # o: (batch, seqlen, hidden)
         return o, a
 
 class PositionwiseFFN(nn.Module):
-    def __init__(self, n_hidden, f_hidden, dropout=0.1):
+    def __init__(self, n_hiddens, f_hiddens, dropout):
         super().__init__()
-        self.w_1 = nn.Linear(n_hidden, f_hidden)
-        self.w_2 = nn.Linear(f_hidden, n_hidden)
+        self.w_1 = nn.Linear(n_hiddens, f_hiddens)
+        self.w_2 = nn.Linear(f_hiddens, n_hiddens)
         self.dropout = nn.Dropout(dropout)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         # x: (batch, seqlen, hidden)
         x = self.relu(self.w_1(x))
-        # x: (batch, seqlen, f_hidden)
+        # x: (batch, seqlen, f_hiddens)
         x = self.w_2(self.dropout(x))
         # x: (batch, seqlen, hidden)
         return x
 
 class AddNorm(nn.Module):
-    def __init__(self, n_shape, dropout=0.1):
+    def __init__(self, n_shape, dropout):
         super().__init__()
         self.norm = nn.LayerNorm(n_shape)
         self.dropout = nn.Dropout(dropout)
@@ -62,12 +62,12 @@ class AddNorm(nn.Module):
         return self.norm(x + self.dropout(y))
 
 class EncoderLayer(nn.Module):
-    def __init__(self, n_head, n_hidden, ff_hidden, dropout=0.1):
+    def __init__(self, n_heads, n_hiddens, ff_hiddens, dropout):
         super().__init__()
-        self.att = MultiHeadAttention(n_hidden, n_head, dropout)
-        self.att_norm = AddNorm(n_hidden, dropout)
-        self.ffn = PositionwiseFFN(n_hidden, ff_hidden, dropout)
-        self.ffn_norm = AddNorm(n_hidden, dropout)
+        self.att = MultiHeadAttention(n_hiddens, n_heads, dropout)
+        self.att_norm = AddNorm(n_hiddens, dropout)
+        self.ffn = PositionwiseFFN(n_hiddens, ff_hiddens, dropout)
+        self.ffn_norm = AddNorm(n_hiddens, dropout)
 
     def forward(self, x, mask):
         x_, _ = self.att(x, x, x, mask)
@@ -77,32 +77,32 @@ class EncoderLayer(nn.Module):
         return x
 
 class DecoderLayer(nn.Module):
-    def __init__(self, n_head, n_hidden, ff_hidden, dropout=0.1):
+    def __init__(self, n_heads, n_hiddens, ff_hiddens, dropout):
         super().__init__()
-        self.att = MultiHeadAttention(n_hidden, n_head, dropout)
-        self.att_norm = AddNorm(n_hidden, dropout)
-        self.attE = MultiHeadAttention(n_hidden, n_head, dropout)
-        self.attE_norm = AddNorm(n_hidden, dropout)
-        self.ffn = PositionwiseFFN(n_hidden, ff_hidden, dropout)
-        self.ffn_norm = AddNorm(n_hidden, dropout)
+        self.att = MultiHeadAttention(n_hiddens, n_heads, dropout)
+        self.att_norm = AddNorm(n_hiddens, dropout)
+        self.attE = MultiHeadAttention(n_hiddens, n_heads, dropout)
+        self.attE_norm = AddNorm(n_hiddens, dropout)
+        self.ffn = PositionwiseFFN(n_hiddens, ff_hiddens, dropout)
+        self.ffn_norm = AddNorm(n_hiddens, dropout)
 
-    def forward(self, x, enc_x, mask, enc_mask):
-        x_, _ = self.att(x, x, x, mask)
-        x = self.att_norm(x, x_)
-        x_, _ = self.attE(x, enc_x, enc_x, enc_mask)
-        x =  self.attE_norm(x, x_)
-        x_ = self.ffn(x)
-        x = self.ffn_norm(x, x_)
-        return x
+    def forward(self, y, enc_x, mask, enc_mask):
+        y_, _ = self.att(y, y, y, mask)
+        y = self.att_norm(y, y_)
+        y_, _ = self.attE(y, enc_x, enc_x, enc_mask)
+        y =  self.attE_norm(y, y_)
+        y_ = self.ffn(y)
+        y = self.ffn_norm(y, y_)
+        return y
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, n_vocab, n_layers, n_head, n_hidden, ff_hidden,
-                 n_pos=100, dropout=0.1):
+    def __init__(self, n_vocab, n_layers, n_heads, n_hiddens, ff_hiddens,
+                 n_position=100, dropout=0.1):
         super().__init__()
-        self.tok_emb = nn.Embedding(n_vocab, n_hidden)
-        self.pos_emb = nn.Embedding(n_pos, n_hidden)
+        self.tok_emb = nn.Embedding(n_vocab, n_hiddens)
+        self.pos_emb = nn.Embedding(n_position, n_hiddens)
         self.layers = nn.ModuleList([
-            EncoderLayer(n_head, n_hidden, ff_hidden, dropout)
+            EncoderLayer(n_heads, n_hiddens, ff_hiddens, dropout)
             for _ in range(n_layers)])
         self.dropout = nn.Dropout(dropout)
 
@@ -118,15 +118,15 @@ class TransformerEncoder(nn.Module):
         return x
 
 class TransformerDecoder(nn.Module):
-    def __init__(self, n_vocab, n_layers, n_head, n_hidden, ff_hidden,
-                 n_pos=100, dropout=0.1):
+    def __init__(self, n_vocab, n_layers, n_heads, n_hiddens, ff_hiddens,
+                 n_position=100, dropout=0.1):
         super().__init__()
-        self.tok_emb = nn.Embedding(n_vocab, n_hidden)
-        self.pos_emb = nn.Embedding(n_pos, n_hidden)
+        self.tok_emb = nn.Embedding(n_vocab, n_hiddens)
+        self.pos_emb = nn.Embedding(n_position, n_hiddens)
         self.layers = nn.ModuleList([
-            DecoderLayer(n_head, n_hidden, ff_hidden, dropout)
+            DecoderLayer(n_heads, n_hiddens, ff_hiddens, dropout)
             for _ in range(n_layers)])
-        self.dense = nn.Linear(n_hidden, n_vocab)
+        self.dense = nn.Linear(n_hiddens, n_vocab)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, enc_x, mask, enc_mask):
@@ -145,17 +145,17 @@ class TransformerSeq2Seq(nn.Module):
         super().__init__()
         self.encoder = TransformerEncoder(n_vocab=enc_vocab,
                                           n_layers=kw.get('n_layers', 2),
-                                          n_head=kw.get('n_head', 4),
-                                          n_hidden=kw.get('n_hidden', 512),
-                                          ff_hidden=kw.get('ff_hidden', 1024),
-                                          n_pos=kw.get('n_pos', 100),
+                                          n_heads=kw.get('n_heads', 4),
+                                          n_hiddens=kw.get('n_hiddens', 512),
+                                          ff_hiddens=kw.get('ff_hiddens', 1024),
+                                          n_position=kw.get('n_position', 100),
                                           dropout=kw.get('dropout', 0.0))
         self.decoder = TransformerDecoder(n_vocab=dec_vocab,
                                           n_layers=kw.get('n_layers', 2),
-                                          n_head=kw.get('n_head', 4),
-                                          n_hidden=kw.get('n_hidden', 512),
-                                          ff_hidden=kw.get('ff_hidden', 1024),
-                                          n_pos=kw.get('n_pos', 100),
+                                          n_heads=kw.get('n_heads', 4),
+                                          n_hiddens=kw.get('n_hiddens', 512),
+                                          ff_hiddens=kw.get('ff_hiddens', 1024),
+                                          n_position=kw.get('n_position', 100),
                                           dropout=kw.get('dropout', 0.0))
 
     def make_enc_mask(self, x, x_len):
@@ -182,7 +182,7 @@ class TransformerSeq2Seq(nn.Module):
         return out
 
 if __name__ == '__main__':
-    seq2seq = TransformerSeq2Seq(101, 102, n_layers=2, n_head=4)
+    seq2seq = TransformerSeq2Seq(101, 102, n_layers=2, n_heads=4)
     enc_x, enc_len = torch.randint(101, (8, 10)), torch.randint(1, 10, (8,))
     dec_x, dec_len = torch.randint(102, (8, 11)), torch.randint(1, 11, (8,))
     outs = seq2seq(enc_x, enc_len, dec_x, dec_len)
