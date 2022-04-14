c84dbc164faa992d78bfc0797155f5192bb907c3
Chen Gui <gui.g.chen@gmail.com>
2022-04-14 22:47:02 +0800

    Fix beam search pruning
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- torch-nmt/nmt/model/beam.py
+++ torch-nmt/nmt/model/beam.py
@@ -21,8 +21,25 @@ def beam_search(out, preds, scores, beam):
     scores, b_indics = scores.view(bs, -1).topk(beam, dim=-1)
     # scores: (batch, beam), (batch, beam)
     b_row, b_col = b_indics.div(beam).long(), b_indics.fmod(beam).long()
-    batch = torch.arange(bs).unsqueeze(1).repeat(1, beam)
-    index = indics[batch.view(-1), b_row.view(-1), b_col.view(-1)].long()
+    batch = torch.arange(bs).unsqueeze(1).repeat(1, beam).view(-1)
+    index = indics[batch, b_row.view(-1), b_col.view(-1)].long()
     # index: (batch*beam,)
+    preds = preds[batch, b_row.view(-1), :].view(bs, beam, -1)
     preds = torch.cat([preds, index.view(bs, beam, 1)], dim=-1)
     return preds, scores
+
+
+if __name__ == '__main__':
+    from torch.nn import functional as F
+
+    n_steps, n_batch, n_vocab, n_beam = 5, 2, 3, 2
+    for t in range(n_steps):
+        batch_size = n_batch if t == 0 else n_batch*2
+        out = F.softmax(torch.randn(batch_size, 1, n_vocab), dim=-1)
+        print(out)
+        if t == 0:
+            preds, scores = beam_initial(out, n_beam)
+        else:
+            preds, scores = beam_search(out, preds, scores, n_beam)
+        print(preds)
+    print(preds)
